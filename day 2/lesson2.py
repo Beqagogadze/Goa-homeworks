9ac3f05800e16ee40af4f74707607315f6a0f3b8
Beqagogadze <Beqaghoghadze777@gmail.com>
2024-06-27 18:14:26 +0400

    first commit

---
+++ day 2/lesson2.py
@@ -0,0 +1,14 @@
+# var - variable - ცვლადი
+var1 = "Goal-Oriented Academy"
+
+#str1 = "beqa"
+#str2 = "beqa"
+ 
+
+num1 = int(input("enter your number: "))
+num2 = int(input("enter your number: "))
+num3 = int(input("enter your number: "))
+ 
+
+
+
---
+++ day 4/classwork.py
@@ -0,0 +1,20 @@
+#num1 = int(input("enter first number: "))
+#num2 = int(input("enter second number: "))
+#num1 = int(input("enter third number: "))
+
+
+#name = input("enter your name: ")
+#Lastname = input("enter your last name: ")
+
+
+
+
+name = input("Beqa: ")
+
+lastname = input("Gogadze: ")
+
+gmail = input("BeqaGogadze033@gmail.com: ")
+
+password = input("343254434: ")
+
+print()
